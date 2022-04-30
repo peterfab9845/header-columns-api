c21343513d284cdc046f2844611fa2dda0d507eb
Peter Fabinski <git@peterfab.com>
2022-04-30 03:14:06 -0400

    Migrate all custom columns out of window subscript

--- implementation.js
+++ implementation.js
@@ -4,19 +4,16 @@
 // via the function argument instead.
 (function (exports) {
 
-  var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+  var { AppConstants } = ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
   var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
   var { ExtensionSupport } = ChromeUtils.import("resource:///modules/ExtensionSupport.jsm");
+  var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
   class ColumnHandler {
-    constructor(parseTree, sortNumeric) {
+    constructor(win, parseTree, sortNumeric) {
+      this.win = win;
       this.parseTree = parseTree;
       this.sortNumeric = sortNumeric;
-    }
-
-    // Access to the window object
-    init(win) {
-      this.win = win;
     }
 
     // Required (?) custom column handler functions are according to
@@ -128,10 +125,196 @@
     }
   }
 
-  // List of columns managed by this instance of the experiment, passed
-  // directly to the customcol js. To update a column, change it in the map
-  // here and then notify customcols to reread the map.
-  const managedColumns = new Map();
+  class ColumnManager {
+    constructor(extension) {
+      this.extension = extension;
+      this.managedColumns = new Map();
+      this.managedWindows = new Map();
+    }
+
+    get instanceId() {
+      return `${this.extension.uuid}-${this.extension.instanceId}`;
+    }
+
+    get windowListenerId() {
+      return `header-columns-${this.instanceId}`;
+    }
+
+    // Set up elements (and possibly handlers) for all columns in a new window.
+    onLoadWindow(win) {
+      if (win.document.documentElement.getAttribute("windowtype") != "mail:3pane")
+        return;
+
+      this.managedWindows.set(win, {
+        "handlers": new Set(),
+        "elements": new Set()
+      });
+
+      // Handlers are usually added by the MsgCreateDBView observer, but
+      // we may have missed the event and need to do it ourselves.
+      if (win.gDBView) {
+        for (const id of this.managedColumns.keys()) {
+          this.addHandler(win, id);
+        }
+      }
+
+      for (const id of this.managedColumns.keys()) {
+        this.addTreeCol(win, id);
+      }
+    }
+
+    // Set up handlers for all columns in some window's new DBView.
+    onCreateDBView() {
+      for (const win of this.managedWindows.keys()) {
+        for (const id of this.managedColumns.keys()) {
+          this.addHandler(win, id);
+        }
+      }
+    }
+
+    // Clean up handlers and elements for all columns in a window.
+    onUnloadWindow(win) {
+      this.managedWindows.delete(win);
+      // Handlers and elements are going away anyway, no need to remove.
+    }
+
+    // Add handlers and elements for a column to all windows.
+    onRegisterColumn(id, label, tooltip, parseTree, sortNumeric) {
+      this.managedColumns.set(id, {
+        "label": label,
+        "tooltip": tooltip,
+        "parseTree": parseTree,
+        "sortNumeric": sortNumeric
+      });
+      for (const win of this.managedWindows.keys()) {
+        this.addHandler(win, id);
+        this.addTreeCol(win, id);
+      }
+    }
+
+    // Remove handlers and elements for a column from all windows.
+    onUnregisterColumn(id) {
+      this.managedColumns.delete(id);
+      for (const win of this.managedWindows.keys()) {
+        this.removeHandler(win, id);
+        this.removeTreeCol(win, id);
+      }
+    }
+
+    // Add a column handler to a given window's DBView to be used by message
+    // tree elements. Can be used to replace existing custom handlers.
+    addHandler(win, id) {
+      // If there's no gDBView, we can't do anything in this window yet. Wait
+      // to get called again by the MsgCreateDBView observer.
+      if (!win.gDBView) return;
+
+      const col = this.managedColumns.get(id);
+      if (!col) return;
+
+      try {
+        // We need a new instance of the handler for each window, because
+        // each one needs a window reference to access message data.
+        let handler = new ColumnHandler(win, col.parseTree, col.sortNumeric);
+        win.gDBView.addColumnHandler(id, handler);
+      } catch (ex) {
+        console.error(ex);
+        throw new Error(`Cannot add column handler for column ID ${id}`);
+      }
+      this.managedWindows.get(win).handlers.add(id);
+    }
+
+    // Remove a column handler from a given window's DBView.
+    removeHandler(win, id) {
+      if (!win.gDBView) return;
+      if (!this.managedWindows.get(win).handlers.has(id)) return;
+
+      try {
+        win.gDBView.removeColumnHandler(id);
+      } catch (ex) {
+        console.error(ex);
+        throw new Error(`Cannot remove column handler for column ID ${id}`);
+      }
+      this.managedWindows.get(win).handlers.delete(id);
+    }
+
+    // Add a column element to a given window's message tree view. Can be used
+    // to replace existing custom columns.
+    addTreeCol(win, id) {
+      // remove an old column if it already exists
+      this.removeTreeCol(win, id);
+
+      const col = this.managedColumns.get(id);
+      if (!col) return;
+
+      // create element
+      const treeCol = win.document.createXULElement("treecol");
+      treeCol.setAttribute("id", id);
+      treeCol.setAttribute("persist", "hidden ordinal sortDirection width");
+      treeCol.setAttribute("flex", "2");
+      treeCol.setAttribute("closemenu", "none");
+      treeCol.setAttribute("label", col.label);
+      treeCol.setAttribute("tooltiptext", col.tooltip);
+
+      // add element
+      const threadCols = win.document.getElementById("threadCols");
+      threadCols.appendChild(treeCol);
+
+      // Restore persisted attributes.
+      const attributes = Services.xulStore.getAttributeEnumerator(
+        win.document.URL,
+        id
+      );
+      for (const attribute of attributes) {
+        const value = Services.xulStore.getValue(win.document.URL, id, attribute);
+        // See Thunderbird bug 1607575 and bug 1612055.
+        if (attribute != "ordinal" || parseInt(AppConstants.MOZ_APP_VERSION, 10) < 74) {
+          treeCol.setAttribute(attribute, value);
+        } else {
+          treeCol.ordinal = value;
+        }
+      }
+
+      this.managedWindows.get(win).elements.add(id);
+    }
+
+    // Remove a column element from a given window's message tree view.
+    removeTreeCol(win, id) {
+      if (!this.managedWindows.get(win).elements.has(id)) return;
+
+      const treeCol = win.document.getElementById(id);
+      if (!treeCol) return;
+
+      treeCol.remove();
+      this.managedWindows.get(win).elements.delete(id);
+    }
+
+    // Remove all handlers and elements from all managed windows.
+    removeAll() {
+      for (const [win, state] of this.managedWindows) {
+        for (const id of state.handlers) {
+          this.removeHandler(win, id);
+        }
+        for (const id of state.elements) {
+          this.removeTreeCol(win, id);
+        }
+      }
+      this.managedWindows.clear();
+    }
+  }
+
+  var manager;
+
+  var createDBViewObserver = {
+    observe(aMsgFolder, aTopic, aData) {
+      manager.onCreateDBView();
+    },
+    register() {
+      Services.obs.addObserver(this, "MsgCreateDBView", false);
+    },
+    unregister() {
+      Services.obs.removeObserver(this, "MsgCreateDBView");
+    }
+  };
 
   class HeaderColumns extends ExtensionCommon.ExtensionAPI {
     // Construct an instance of our experiment; called once (per addon using the
@@ -141,27 +324,22 @@
       // Note: super() sets our this.extension member to the extension.
       super(...args);
 
+      manager = new ColumnManager(this.extension);
+
+      createDBViewObserver.register();
+
       ExtensionSupport.registerWindowListener(
-        `header-columns-${this.extension.uuid}-${this.extension.instanceId}`,
+        manager.windowListenerId,
         {
-          extension: this.extension, // TODO ugly, use a class containing the functions instead
           chromeURLs: [
             "chrome://messenger/content/messenger.xul",
             "chrome://messenger/content/messenger.xhtml"
           ],
-          onLoadWindow: function(win) {
-            // FIXME this setup all overwrites an existing CustomColumns object if multiple instances of the API exist
-            // keep only one loaded somehow?
-            win.CustomColumns = {};
-            Services.scriptloader.loadSubScript(this.extension.getURL("api/header-columns-api/customcol.js"), win.CustomColumns);
-            win.CustomColumns.managedColumns = managedColumns;
-            win.CustomColumns.CustomColumnsView.init(win);
+          onLoadWindow(win) {
+            manager.onLoadWindow(win);
           },
-          onUnloadWindow: function(win) {
-            // FIXME this causes an error on window close if multiple instances of the API exist
-            // how do we know whether to destroy or not?
-            win.CustomColumns.CustomColumnsView.destroy();
-            delete win.CustomColumns;
+          onUnloadWindow(win) {
+            manager.onUnloadWindow(win);
           }
         }
       );
@@ -172,7 +350,9 @@
     onShutdown(isAppShutdown) {
       if (isAppShutdown) return; // Everything is going away anyway.
 
-      ExtensionSupport.unregisterWindowListener(`header-columns-${this.extension.uuid}-${this.extension.instanceId}`);
+      ExtensionSupport.unregisterWindowListener(manager.windowListenerId);
+      createDBViewObserver.unregister();
+      manager.removeAll();
 
       // Looks like we got uninstalled. Maybe a new version will be installed now.
       // Due to new versions not taking effect (https://bugzilla.mozilla.org/show_bug.cgi?id=1634348)
@@ -188,17 +368,10 @@
       return {
         HeaderColumns: {
           registerColumn(id, label, tooltip, parseTree, sortNumeric) {
-            let handler = new ColumnHandler(parseTree, sortNumeric);
-            managedColumns.set(id, {
-              "label": label,
-              "tooltip": tooltip,
-              "handler": handler
-            });
-            Services.obs.notifyObservers(null, "CustomColumns:column-updated", id);
+            manager.onRegisterColumn(...arguments);
           },
           unregisterColumn(id) {
-            managedColumns.delete(id);
-            Services.obs.notifyObservers(null, "CustomColumns:column-updated", id);
+            manager.onUnregisterColumn(id);
           }
         }
       };
